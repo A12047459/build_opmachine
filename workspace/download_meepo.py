c9a9db60dcc98adfbf501a95a87159adad2182e5
黄尚学 <53204527+A12047459@users.noreply.github.com>
2022-02-15 13:57:36 +0800

    Create download_meepo.py

---
+++ workspace/download_meepo.py
@@ -0,0 +1,12 @@
+import requests
+
+def rq_download(url,path,name):
+    r = requests.get(url)
+    with open(path+name, 'wb') as code:
+            code.write(r.content)
+
+if __name__ == '__main__':
+    url = 'https://github.com/peerXu/meepo/releases/download/0.7.1/meepo_linux_amd64.tar.gz'
+    path = './package/'
+    name = 'meepo_linux_amd64.tar.gz'
+    rq_download(url, path, name)
